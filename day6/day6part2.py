5690656beb02b27ce438ba8606c8cf9fdddc1b3f
Jeff Glass <glass.jeffrey@gmail.com>
2024-12-09 19:02:45 -0800

    day 6 part 2

--- day6/day6part2.py
+++ day6/day6part2.py
@@ -25,25 +25,45 @@ def inbound(l, c, max_line, max_chars):
 
 if __name__ == '__main__':
     with open('day6/data.txt', 'r') as f:
-        obstacles, location, num_lines, num_chars = load_data(f.readlines())
-    
-    occupied: set[tuple[Position, Direction]] = set()
-    direction = Direction.NORTH
+        obstacles, start_location, num_lines, num_chars = load_data(f.readlines())
 
-    while inbound(*location, num_lines, num_chars):
-        occupied.add(location)
-        next_location = (location[0] + direction.value[0], location[1] + direction.value[1])
-        if next_location in obstacles:
-            match direction:
-                case Direction.NORTH: # NORTH
-                    direction = Direction.EAST # EAST
-                case  Direction.EAST:
-                    direction = Direction.SOUTH
-                case  Direction.SOUTH:
-                    direction = Direction.WEST
-                case  Direction.WEST:
-                    direction = Direction.NORTH
-        else:
-            location = next_location
+    count = 0
+    for l in range(num_lines):
+        for c in range(num_chars):
+            location = start_location
+            if (l, c) in obstacles:
+                #print(f"{str((l, c)): <10} Already an obstacle here")
+                continue
+            if location == (l, c):
+                #print(f"{str((l, c)): <10} Guard starts here!")
+                continue # Can't add a new obstacle here
+            current_obstacles = obstacles | {(l, c)}
+            ##print(current_obstacles)
+
+            occupied: set[tuple[Position, Direction]] = set()
+            direction = Direction.NORTH
+
+            while inbound(*location, num_lines, num_chars):
+                if (location, direction) in occupied: #already been here
+                    print(f"{str((l, c)): <10} Found a loop! ")
+                    count += 1
+                    break
+                occupied.add((location, direction))
+                next_location = (location[0] + direction.value[0], location[1] + direction.value[1])
+                if next_location in current_obstacles:
+                    match direction:
+                        case Direction.NORTH: # NORTH
+                            direction = Direction.EAST # EAST
+                        case  Direction.EAST:
+                            direction = Direction.SOUTH
+                        case  Direction.SOUTH:
+                            direction = Direction.WEST
+                        case  Direction.WEST:
+                            direction = Direction.NORTH
+                else:
+                    location = next_location
+            else:
+                pass
+                #print(f"{str((l, c)): <10} Guard exited, no loop found")
     
-    print(f"{len(occupied)= }")
+    print(f"{count= }")

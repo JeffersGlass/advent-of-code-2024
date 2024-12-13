05731a282c109719251745ff246c8dce5ee5c601
Jeff Glass <glass.jeffrey@gmail.com>
2024-12-12 21:15:02 -0800

    Working formatting

--- day8/t.py
+++ day8/t.py
@@ -1,14 +1,28 @@
 from textual.app import App, ComposeResult
 from textual.containers import HorizontalGroup
 from textual.reactive import reactive
-from textual.widgets import Footer, Header, Label
+from textual.widgets import Footer, Header, Label, Static
+from textual.widget import Widget
 
 class PositionLabel(Label):
-    has_antenna = reactive(False)
-    has_antinode = reactive(False)
 
-    def compose(self):
-        r
+    def __init__(self, antenna = False, antinode = False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.has_antenna = reactive(antenna)
+        self.has_antinode = reactive(antinode)
+        self.add_class("position")
+
+    def watch_has_antenna(self, has_antenna: bool):
+        if has_antenna:
+            self.add_class("antenna")
+        else:
+            self.remove_class("antenna")
+
+    def watch_has_antinode(self, has_antinode: bool):
+        if has_antinode:
+            self.add_class("antinode")
+        else:
+            self.remove_class("antinode")
 
 class AntennaApp(App):
 
@@ -16,9 +30,17 @@ class AntennaApp(App):
     
     def compose(self) -> ComposeResult:
         yield Header()
-        for i in range(10):
+        for i in range(2):
             yield(HorizontalGroup(
-                *[PositionLabel(str(i), classes="position", name=f"({i},{j})") for j in range(10)]  
+                *[PositionLabel(renderable=str(i), classes="", name=f"({i},{j})") for j in range(12)]  
+            ))
+        for i in range(3):
+            yield(HorizontalGroup(
+                *[PositionLabel(renderable=str(i), classes="antinode", name=f"({i},{j})") for j in range(12)]  
+            ))
+        for i in range(4):
+            yield(HorizontalGroup(
+                *[PositionLabel(renderable=str(i), classes="antenna", name=f"({i},{j})") for j in range(10)]  
             ))
         yield Footer()
 

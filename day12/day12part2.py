e9d06c2839692b922431d0b60b82b730d92ec085
Jeff Glass <glass.jeffrey@gmail.com>
2024-12-16 17:23:31 -0600

    More work with errors

--- day12/day12part2.py
+++ day12/day12part2.py
@@ -1,6 +1,7 @@
 from collections import UserDict
 from collections.abc import Container, Sized, Iterable, Hashable, MutableSet
 from itertools import chain
+from operator import itemgetter
 from pathlib import Path
 from typing import Iterator, Self, Generator, Iterable, Any
 from functools import cached_property
@@ -9,6 +10,9 @@ class Side(MutableSet, Hashable):
     def __init__(self, initial_value: Iterable | None = None):
         self.segments: set[tuple[float, float]] = set()
         if initial_value is not None: self.segments = set(initial_value)
+
+    def __str__(self) -> str:
+        return f"Side: {self.segments}"
 
     def __contains__(self, x: object) -> bool:
         return self.segments.__contains__(x)
@@ -28,11 +32,27 @@ class Side(MutableSet, Hashable):
     def discard(self, value: Any) -> None:
         return self.segments.discard(value)
 
-    def matches(self, seg: tuple[float, float]):
+    def matches(self, seg: tuple[float, float]) -> bool:
         if seg in self.segments: return True
-        for delta in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-            if (seg[0] + delta[0], seg[1], + delta[1]) in self.segments: return True
+        for delta in -1, 1:
+            other = (seg[0], seg[1] + delta)
+            if seg[1] == int(seg[1]) and other in self.segments: return True
+        for delta in -1, 1:
+            other = (seg[0] + delta, seg[1])
+            if seg[0] == int(seg[0]) and other in self.segments: return True
         return False
+    
+def test_side():
+    s = Side([(1, 2.5)])
+    assert s.matches((1,2.5))
+    assert not s.matches((1,3.5))
+    assert s.matches((2, 2.5))
+
+    t = Side([(0.5, 0)])
+    assert t.matches((0.5, 0))
+    assert not t.matches((1.5, 0))
+    assert t.matches((0.5, 1))
+
 
 class Region(Container, Sized, Iterable, Hashable):
     def __init__(self, label: str, positions: Iterable[tuple[int, int]] | None = None):
@@ -63,30 +83,49 @@ class Region(Container, Sized, Iterable, Hashable):
     def remove(self, o:tuple[int, int]):
         self.positions.remove(o)
 
-    @cached_property
+    @property
     def area(self) -> int:
         return len(self)
     
-    @cached_property
+    @property
     def num_sides(self) -> int:
         self.sides = set()
         for seg in self.side_segments():
+            found_match = False
             for side in self.sides:
                 if side.matches(seg):
+                    #print(f"Adding segment {seg} to side {side}")
                     side.add(seg)
-                    break
-            self.sides.add(Side([seg]))
+                    found_match = True
+                    #break
+            if not found_match:
+                #print(f"Starting new side with seg {seg}")
+                self.sides.add(Side([seg]))
 
+        print("\n".join(str(s) for s in self.sides))
         return len(self.sides)
     
     def side_segments(self) -> Generator[tuple[float, float]]:
         for p in self:
             for n in Map.neighbors_unrestricted(p):
-                if n not in p:
+                if n not in self:
                     yield ((p[0]+n[0])/2, (p[1]+n[1])/2)
     
     def price_in_map(self, map: "Map") -> int:
         return self.area * self.num_sides
+    
+def test_region():
+    r = Region("a", [(0, 0)])
+    assert r.area == 1
+    assert r.num_sides == 4
+    s = Region("b", [(0, 0), (0, 1)])
+    assert s.area == 2
+    assert s.num_sides == 4
+    t = Region("c", [(0,0), (0, 1), (1,0)])
+    assert t.area == 3
+    assert t.num_sides == 6
+    
+    
 
 class Map(UserDict):
     def __init__(self, max_lines:int, max_chars:int, *args, **kwargs):
@@ -175,7 +214,7 @@ class Map(UserDict):
 
 if __name__ == "__main__":
     myMap = Map.from_path("day12/datatest.txt")
-    print(myMap)
+    #print(myMap)
     for r in myMap.regions:
-        print(f"{r} Num_Sides:{r.num_sides} Price:{r.price_in_map(myMap)}")
+        print(f"{r} Num_Sides:{r.num_sides}")# Price:{r.price_in_map(myMap)}")
     print(f"Total cost: {sum(r.price_in_map(myMap) for r in myMap.regions)}")

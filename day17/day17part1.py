f556280b45214a593452e1e854b6f4dfbc2dfe8f
Jeff Glass <glass.jeffrey@gmail.com>
2024-12-17 12:58:06 -0600

    Day 17 part 2 - printing but not solving sample data

--- day17/day17part1.py
+++ day17/day17part1.py
@@ -4,16 +4,30 @@ import logging
 import re
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    with open("day17/data.txt", "r") as f:
+    #logging.basicConfig(level=logging.DEBUG)
+    with open("day17/datatest.txt", "r") as f:
         data = f.read()
 
     a = re.search(r"Register A: (\d+)", data)
     b = re.search(r"Register B: (\d+)", data)
     c = re.search(r"Register C: (\d+)", data)
     prog = re.search(r"Program: ([\d,]+)", data)
+    initial_prog = ','.join(s for s in prog.group(1).split(","))
+    print(initial_prog)
 
-    c = Computer((int(s) for s in prog.group(1).split(",")), (int(a.group(1)), int(b.group(1)), int(c.group(1))))
-    print(str(c))
-    c.run()
-    print(c.output)
+    a = 1
+    while True:
+        comp = Computer(
+            [int(s) for s in prog.group(1).split(",")],
+            (a,
+            int(b.group(1)),
+            int(c.group(1))),
+        )
+        #print(comp)
+        comp.run()
+        if comp.output == ",".join(str(s) for s in initial_prog):
+            print(f"Smallest A: {a}")
+            break
+        a += 1
+        if not a % 1000:
+            print(a)
